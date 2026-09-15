671f23e01192d0ea242f81c4f6158ca91e648e1d
aphecks148-svg <aphecks148@gmail.com>
2026-09-15 07:03:49 +0000

    Create package.js

---
+++ package.js
@@ -0,0 +1,25 @@
+{
+  "name": "killer-bot-400",
+  "version": "1.0.0-final",
+  "description": "Killer Bot 400 cmds - 8 files x 50 - Owner Aphecks iKon Klerk 100086783504073 - Pets 30 Dangerous Divine+ - Bars - LVL 1000 Prestige - Pokemon 20 every 20min - Auto Reacts - Never Sleep - Canvas + ws3 fca + express + render",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "nodemon index.js"
+  },
+  "engines": {
+    "node": ">=18.0.0"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "fca-priyansh": "^1.0.0",
+    "fca-unofficial": "^0.1.0",
+    "canvas": "^2.11.2",
+    "axios": "^1.6.7",
+    "dotenv": "^16.3.1",
+    "ws3-fca": "^1.0.0"
+  },
+  "keywords": ["bot", "400 cmds", "economy", "pets", "pokemon", "bars", "prestige", "level 1000", "canvas", "ws3", "fca", "render"],
+  "author": "Ahmed Khan <100086783504073>",
+  "license": "MIT"
+}
